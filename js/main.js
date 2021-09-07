6ee0fb9ec6abf79d2badf17be3e79ab694b9cf3d
emrebezci <emrebezci@protonmail.com>
2021-09-07 05:13:21 +0300

    onclick open isactive menu by change display added

--- js/main.js
+++ js/main.js
@@ -29,3 +29,33 @@ function hamburgerclose(){
         close.style.display = "block";
     }
 }
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+var closeimg = document.getElementsByClassName("arrowimg");
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        
+        var content = this.nextElementSibling;
+        
+        if (content.style.display === "block") {
+            
+            content.style.display = "none";
+        } else {
+            
+            content.style.display = "block";
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
